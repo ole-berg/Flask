2e3e103ff8db319eeeac17c1ff1f7f2a5e9de42c
ole-berg <55295828+ole-berg@users.noreply.github.com>
2023-02-18 11:21:18 +0100

    further expansion

--- app.py
+++ app.py
@@ -3,9 +3,22 @@ import os
 from flask import Flask, render_template, request, redirect, url_for
 from markupsafe import escape
 import flask_monitoringdashboard as dashboard
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 dashboard.bind(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///wta.db"
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/", methods=['GET'])
@@ -23,29 +36,33 @@ def hello_html(name):
     return render_template("hello.html", name=name)
 
 
-@app.route("/createUser", methods=['GET', 'POST'])
+@app.route("/user/create", methods=['GET', 'POST'])
 def create_user():
     if request.method == 'GET':
-        return render_template("form.html")
+        return render_template("user/form.html")
     else:
-        return f"<h1>Benutzer mit dem Benutzernamen {escape(request.form['username'])} angelegt</h1>"
+        if not user_exists(request.form['username']):
+            print(user_exists(request.form['username']))
+            add_user(request.form['username'], request.form['password'])
+            return f"<h1>Benutzer mit dem Benutzernamen {escape(request.form['username'])} angelegt</h1>", 200
+        else:
+            return "Ein Benutzer mit dem gleichen Benutzernamen existiert bereits", 400
 
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'GET' or request.files['file'] is None:
-        return render_template('upload.html')
-    else:
-        uploaded_file = request.files['file']
-        if uploaded_file.filename != '':
-            uploaded_file.save(uploaded_file.filename)
-            return redirect(url_for('show', file_name=uploaded_file.filename))
+@app.route("/user/<int:id>")
+def user_detail(id):
+    user = db.get_or_404(User, id)
+    return render_template("user/detail.html", user=user)
 
 
-@app.route('/photo/<file_name>')
-def show(file_name):
-    full_filename = "../"+file_name
-    return render_template('img.html', file_name=file_name)
+def add_user(username, password):
+    db.session.add(User(username=username, password=password))
+    db.session.commit()
+
+
+def user_exists(username):
+    result = db.session.execute(db.select(User).filter_by(username=username)).scalars()
+    return result.first() is not None
 
 
 if __name__ == '__main__':
